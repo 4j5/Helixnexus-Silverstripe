b65f25f9e785e4da675fc19ae86229d5698b7e5e
Andy Smith <andy.helixnexus.uk>
2016-01-28 07:53:03 +0000

    Setup corrections

--- util/db.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE IF NOT EXISTS project_db;
-CREATE USER IF NOT EXISTS 'project_user'@'localhost' IDENTIFIED BY 'secretpassword';
-GRANT ALL ON project_db.* TO 'project_user'@'localhost';
-FLUSH PRIVILEGES;
